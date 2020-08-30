2d1451f9c297df8859497775e6ced85a0d1745df
Jacky Wang <jacky200218@gmail.com>
2020-08-29 23:29:22 -0700

    debg

--- djangoBlog/settings.py
+++ djangoBlog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY=os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['djangomblog.herokuapp.com','127.0.0.1']
 
