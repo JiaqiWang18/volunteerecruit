8cbeb7bf11457a7a59ff712b90424c56abac3d73
Jacky Wang <jacky200218@gmail.com>
2020-10-17 21:25:11 -0700

    display sign ups

--- signups/views.py
+++ signups/views.py
@@ -23,10 +23,16 @@ def create_signup(request,pk):
             # file is saved
             form.instance.post = Post.objects.get(id=pk)
             form.save()
+
             html_message = render_to_string("signups/signup_notice_template.html",{"post_obj":Post.objects.get(id=pk),"form_info":form.instance})
             plain_message = strip_tags(html_message)
             send_mail(f"Your new volunteer sign up for {Post.objects.get(id=pk).title}", plain_message,  os.environ.get("EMAIL_USER"), [form.instance.email],html_message=html_message, fail_silently=False)
-            messages.success(request, f'Your have scuccessfully signed up for {Post.objects.get(id=pk).title}! You will receive an email with the service information.')
+
+            recruiter_html_message=render_to_string("signups/recruiter_signup_notice_template.html",{"post_obj":Post.objects.get(id=pk),"form_info":form.instance})
+            recruiter_plain_message = strip_tags(recruiter_html_message)
+            send_mail(f"You received a new sign up for {Post.objects.get(id=pk).title}", recruiter_plain_message,  os.environ.get("EMAIL_USER"), [Post.objects.get(id=pk).email],html_message=recruiter_html_message, fail_silently=False)
+
+            messages.success(request, f'Your have successfully signed up {Post.objects.get(id=pk).title}! You will receive an email with the service information.')
             return redirect(f'/post/{pk}/')
     else:
         form = NewSignup()
@@ -36,6 +42,10 @@ def create_signup(request,pk):
 class SignupDeleteView(LoginRequiredMixin,UserPassesTestMixin, DeleteView):
     model = SignUp
     success_url = reverse_lazy('my-signups') #redirect to sign up list
+
+    def delete(self, request, *args, **kwargs):
+        messages.warning(self.request, "You have successfully deleted your sign up")
+        return super(SignupDeleteView, self).delete(request, *args, **kwargs)
 
     def test_func(self):
         signup = self.get_object()
@@ -52,6 +62,7 @@ class SignupUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView): #mix
 
     def form_valid(self, form):
         form.instance.author = self.request.user
+        messages.success(self.request, 'Your have successfully updated your sign up')
         return super().form_valid(form)
 
     def test_func(self):
