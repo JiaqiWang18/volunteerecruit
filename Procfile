a08d94e650fe4a72fa6b972cbef401241f8ba077
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-08-29 22:47:30 -0400

    deploy

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 release: python manage.py migrate
 web: gunicorn djangoBlog.wsgi
+celery: celery -A djangoBlog worker -l INFO
--- djangoBlog/celery.py
+++ djangoBlog/celery.py
@@ -9,5 +9,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoBlog.settings')
 app = Celery('djangoBlog')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
+print(app.conf)
 app.autodiscover_tasks()
